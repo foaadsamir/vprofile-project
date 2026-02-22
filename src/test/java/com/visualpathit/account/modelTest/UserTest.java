911d4daf9ac15ab8d5a609f6087881268c65e0c2
fouad samir <foaadsamir1@gmail.com>
2026-02-22 03:01:07 +0200

    Remove unnecessary whitespace in UserTest.java

--- src/test/java/com/visualpathit/account/modelTest/UserTest.java
+++ src/test/java/com/visualpathit/account/modelTest/UserTest.java
@@ -9,19 +9,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String username;
     private String password;
-
     @Transient
     private String passwordConfirm;
-
     @ManyToMany
     @JoinTable(name = "user_role", 
                joinColumns = @JoinColumn(name = "user_id"), 
                inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getUsername() { return username; }
