5f0ee02f6f08d96d6476b2f14dfad93ff219589a
fouad samir <foaadsamir1@gmail.com>
2026-02-22 03:07:17 +0200

    Refactor Role class to use Jakarta Persistence

--- src/main/java/com/visualpathit/account/model/Role.java
+++ src/main/java/com/visualpathit/account/model/Role.java
@@ -1,54 +1,22 @@
 package com.visualpathit.account.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Set;
-/**{@author waheedk} !*/
+
 @Entity
 @Table(name = "role")
 public class Role {
-	/** the id field !*/
-    private Long id;
-    /** the name field !*/
-    private String name;
-    /** the user field !*/
-    private Set<User> users;
-    /** {@inheritDoc}} !*/
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    /**
-     * {@link Role#id}
-     !*/
-    public Long getId() {
-        return id;
-    }
-    /** {@inheritDoc}} !*/
-    public final void setId(final Long id) {
-        this.id = id;
-    }
-    /**
-     * {@link Role#name}
-     !*/
-    public String getName() {
-        return name;
-    }
-    /** {@inheritDoc}} !*/
-    public final void setName(final String name) {
-        this.name = name;
-    }
-    /**
-     * {@inheritDoc}} 
-     !*/
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
     @ManyToMany(mappedBy = "roles")
-    /**
-     * {@link Role#id}
-     !*/
-    public Set<User> getUsers() {
-        return users;
-    }
-    /**
-     * {@inheritDoc}} 
-     !*/
-    public final void setUsers(Set<User> users) {
-        this.users = users;
-    }
+    private Set<User> users;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Set<User> getUsers() { return users; }
+    public void setUsers(Set<User> users) { this.users = users; }
 }
