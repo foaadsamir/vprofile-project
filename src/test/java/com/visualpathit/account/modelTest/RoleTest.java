e0aac4dec8446c9a5d1700222ddb09ef9be3a673
fouad samir <foaadsamir1@gmail.com>
2026-02-22 16:46:12 +0200

    Add RoleTest class with user details tests

--- src/test/java/com/visualpathit/account/modelTest/RoleTest.java
+++ src/test/java/com/visualpathit/account/modelTest/RoleTest.java
@@ -1,24 +1,51 @@
-package com.visualpathit.account.model;
+package com.visualpathit.account.modelTest;
 
-import jakarta.persistence.*;
+import junit.framework.Assert;
+
+import java.util.HashSet;
 import java.util.Set;
 
-@Entity
-@Table(name = "role")
-public class Role {
-    private Long id;
-    private String name;
-    private Set<User> users;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+import com.visualpathit.account.model.Role;
+import com.visualpathit.account.model.User;
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+/** {@author waheedk} !*/
+public class RoleTest {
 
-    @ManyToMany(mappedBy = "roles")
-    public Set<User> getUsers() { return users; }
-    public void setUsers(Set<User> users) { this.users = users; }
+	public static final Long EXPECTED_ID = 1L;	
+    public static final String EXPECTED_ROLENAME = "Admin";
+    public static final int EXPECTED_SIZE = 1;
+    private Role role;
+    @Before
+    public void setUp() throws Exception {
+    	User user = new User();
+        user.setId(1L);
+        user.setUsername("Wahidkhan74");
+        user.setPassword("Wahidkhan74");
+        user.setUserEmail("XXXXX@gmail.com");
+        
+    	Set<User> users = new HashSet<User>();    	
+    	users.add(user);    	
+    	role = new Role();
+    	role.setId(1L);
+        role.setName("Admin");
+        role.setUsers(users);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("Test Completed");
+
+    }
+
+    @Test
+    public void testUserDetailsHappyFlow() throws Exception {
+    	Assert.assertEquals(EXPECTED_ID, role.getId());
+        Assert.assertEquals(EXPECTED_ROLENAME, role.getName());
+        Assert.assertEquals(EXPECTED_SIZE,role.getUsers().size());
+
+    }
 }
