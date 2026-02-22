463cb9fe4e867d6e9cd8d11f7a06a9f71f07beec
fouad samir <foaadsamir1@gmail.com>
2026-02-22 03:01:22 +0200

    Fix formatting and remove unnecessary whitespace

--- src/test/java/com/visualpathit/account/modelTest/RoleTest.java
+++ src/test/java/com/visualpathit/account/modelTest/RoleTest.java
@@ -9,12 +9,9 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
-
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
