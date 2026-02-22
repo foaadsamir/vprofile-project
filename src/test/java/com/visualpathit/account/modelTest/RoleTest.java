0c4c0757a23ff4f266b7622c8d24324381b31cda
fouad samir <foaadsamir1@gmail.com>
2026-02-22 02:57:45 +0200

    Refactor RoleTest to Role entity and remove tests

--- src/test/java/com/visualpathit/account/modelTest/RoleTest.java
+++ src/test/java/com/visualpathit/account/modelTest/RoleTest.java
@@ -1,51 +1,24 @@
-package com.visualpathit.account.modelTest;
+package com.visualpathit.account.model;
 
-import junit.framework.Assert;
-
-import java.util.HashSet;
+import jakarta.persistence.*;
 import java.util.Set;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-import com.visualpathit.account.model.Role;
-import com.visualpathit.account.model.User;
+    private String name;
 
-/** {@author waheedk} !*/
-public class RoleTest {
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
 
-	public static final Long EXPECTED_ID = 1L;	
-    public static final String EXPECTED_ROLENAME = "Admin";
-    public static final int EXPECTED_SIZE = 1;
-    private Role role;
-    @Before
-    public void setUp() throws Exception {
-    	User user = new User();
-        user.setId(1L);
-        user.setUsername("Wahidkhan74");
-        user.setPassword("Wahidkhan74");
-        user.setUserEmail("XXXXX@gmail.com");
-        
-    	Set<User> users = new HashSet<User>();    	
-    	users.add(user);    	
-    	role = new Role();
-    	role.setId(1L);
-        role.setName("Admin");
-        role.setUsers(users);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        System.out.println("Test Completed");
-
-    }
-
-    @Test
-    public void testUserDetailsHappyFlow() throws Exception {
-    	Assert.assertEquals(EXPECTED_ID, role.getId());
-        Assert.assertEquals(EXPECTED_ROLENAME, role.getName());
-        Assert.assertEquals(EXPECTED_SIZE,role.getUsers().size());
-
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Set<User> getUsers() { return users; }
+    public void setUsers(Set<User> users) { this.users = users; }
 }
